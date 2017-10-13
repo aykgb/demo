edca7bf57b51402b3d0269618d424885295325fc
WangChenglin <wangcl.cw@gmail.com>
2017-10-13 07:46:33 -0700

    finish epoll_client coding.
    Signed-off-by: WangChenglin <wangcl.cw@gmail.com>

--- epoll_client.cc
+++ epoll_client.cc
@@ -21,15 +21,69 @@ void setnoblocking(int fd) {
 
 int main(int argc, char *argv[])
 {
-    (void)argv;
-    int sockfd;
-    char recvline[MAX_LINE + 1] = {0};
-
-    struct sockaddr_in server_addr;
-
-    if(argc != 2) {
+   if(argc != 2) {
         fprintf(stderr, "usage: ./client <SERVER_IP>\n");
         exit(0);
+    }
+
+    // 创建socket
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if(sockfd < 0) {
+        fprintf(stderr, "socket error.");
+        exit(-1);
+    }
+
+    // server_addr 赋值
+    struct sockaddr_in server_addr;
+    bzero(&server_addr, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(SERVER_PORT);
+
+    // inet_pton ip 字符串到网络字节流
+    if(inet_pton(AF_INET, argv[1], &server_addr.sin_addr) <= 0) {
+        fprintf(stderr, "inet_pton error for %s.\n", argv[1]);
+        exit(-1);
+    }
+
+    // 连接
+    if(connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+        fprintf(stderr, "connect error.\n");
+        exit(-1);
+    }
+
+    setnoblocking(sockfd);
+
+    char input[100];
+    int n = 0;
+    int count = 0;
+
+    char recvline[MAX_LINE + 1] = {0};
+    while(fgets(input, 100, stdin) != NULL) {
+        printf("[send] %s\n", input);
+        n = 0;
+        n = send(sockfd, input, strlen(input), 0);
+        if(n < 0) {
+            perror("send");
+        }
+
+        n = 0;
+        count = 0;
+
+        while(1) {
+            n = read(sockfd, recvline + count, MAX_LINE);
+            if(n == MAX_LINE) {
+                count += n;
+                continue;
+            } else if(n < 0) {
+                perror("recv");
+                break;
+            } else {
+                count += n;
+                recvline[count] = '\0';
+                printf("[recv] %s\n", recvline);
+                break;
+            }
+        }
     }
 
     return 0;
